eaac2232a17c756969e933ccfaa55c4817838326
yexjin <tnska0421@naver.com>
2021-12-08 00:17:47 +0900

    token 오류 해결

--- front/src/remote/api/exhibitionsApi.js
+++ front/src/remote/api/exhibitionsApi.js
@@ -2,12 +2,12 @@ import urls from '../urls'
 import httpClient from '../httpClient'
 import {getDataFromStorage} from "../../utils/storage";
 
-const accessToken = getDataFromStorage().accessToken;
+const token = getDataFromStorage() === null ? null : getDataFromStorage().accessToken;
 
 const headers = {
-    'Authorization' : `Bearer ${accessToken}`,
+    'Authorization' : `Bearer ${token}`,
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
   }
    
 export const create = (form) => 
-    httpClient.post(`${urls.common}/exhibitions`, form, {headers});
+    httpClient.post(`${urls.common}/exhibitions`, form,{headers});
--- front/src/remote/api/pamphletsApi.js
+++ front/src/remote/api/pamphletsApi.js
@@ -2,11 +2,11 @@ import urls from '../urls'
 import httpClient from '../httpClient'
 import {getDataFromStorage} from "../../utils/storage";
 
-const accessToken = getDataFromStorage().accessToken;
+const token = getDataFromStorage() === null ? null : getDataFromStorage().accessToken;
 
 const headers = {
-    'Authorization' : `Bearer ${accessToken}`,
-  }
- 
+    'Authorization' : `Bearer ${token}`,
+}
+   
 export const create = (form) => 
     httpClient.post(`${urls.common}/pamphlets`, form, {headers});
