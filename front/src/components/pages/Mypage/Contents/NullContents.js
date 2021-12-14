f3117220f6238d7123fbd09b7cdb571344765f05
yexjin <tnska0421@naver.com>
2021-12-15 01:47:03 +0900

    마이페이지 첫로딩 부분 UI수정

--- front/src/components/pages/Mypage/Contents/NullContents.js
+++ front/src/components/pages/Mypage/Contents/NullContents.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import NextImg from '../../../../styles/icons/next2.png'
 
+const Content = styled.div`
+margin-left: 100px;
+margin-top: 100px;
+
+`
+
+const ImgDiv = styled.img`
+transform: rotate( 180deg );
+`
+const Text = styled.div`
+width: 792px;
+font: normal normal normal 18px/30px Roboto;
+letter-spacing: 0px;
+color: #191919;
+opacity: 1;
+`
 function NullContents() {
     return (
-        <div>
-            없음
-        </div>
+        <Content>
+            {/* <ImgDiv src={NextImg} /> */}
+            <Text>전시가 선택되지 않았어요</Text> <br/>
+            <Text>"In progress" 목록에서 진행중인 전시카드를 클릭해주세요 :)</Text>
+        </Content>
     )
 }
 
