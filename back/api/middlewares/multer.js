4ab7dfa3cfd37c36006299361398eabe199ee59c
uijin0531@gmail.com <uijin0531@gmail.com>
2021-12-19 02:40:44 +0900

    이미지 업로드 위치 변경

--- back/api/middlewares/multer.js
+++ back/api/middlewares/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const upload = multer({ storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, '../uploads');
+      cb(null, '../front/public/img/uploads');
     },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}_${file.originalname}`);
